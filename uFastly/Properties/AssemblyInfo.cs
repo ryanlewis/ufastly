2af433de9e7d693d462d735a4f5d81cb17407f11
Ryan Lewis <ryan@wpyz.org>
2016-04-30 02:11:47 +0100

    Better meta

--- uFastly/Properties/AssemblyInfo.cs
+++ uFastly/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("uFastly")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Fastly support for Umbraco")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Ryan McDonough, Ryan Lewis")]
 [assembly: AssemblyProduct("uFastly")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
